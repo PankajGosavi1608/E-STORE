5a68868da0063d1b5b16af31d41f7adad6bec0d3
PankajGosavi1608 <pankajudaygosawi@gmail.com>
2023-06-13 16:39:57 +0530

    Category Module Update on Dev

---
+++ src/main/java/com/mobicool/e/store/service/CategoryService.java
@@ -0,0 +1,27 @@
+package com.mobicool.e.store.service;
+
+import com.mobicool.e.store.dto.CategoryDto;
+import com.mobicool.e.store.dto.PageableResponse;
+
+public interface CategoryService {
+
+    //create
+    CategoryDto create(CategoryDto categoryDto);
+
+    //update
+    CategoryDto update(CategoryDto categoryDto,String categoryId);
+
+    //delete
+    void delete(String categoryId);
+
+    //get all
+
+    PageableResponse<CategoryDto> getAll();
+
+    //get single category by details
+
+    CategoryDto get(String categoryId);
+
+    //search
+
+}
